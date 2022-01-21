36e12884328a79f3f8f0dda1f0da15ce42914c5d
themighty1 <you@example.com>
2022-01-21 17:57:50 +0300

    split c3 into two circuits to enforce same cwk/civ for Client_Finished and the request add write-up about all TLS PRF steps

--- assemble.js
+++ assemble.js
@@ -1,6 +1,6 @@
 const casm = require('./casmbundle');
 global.fs = require('fs'); 
 
-for (let i=1; i<7; i++){
+for (let i=1; i<8; i++){
     global.fs.writeFileSync('c'+i+'.out', casm.parseAndAssemble('c'+i+'.casm'));
 }
